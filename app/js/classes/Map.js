390852c8b0e9347579376e4cb36aacba53c26ab5
Aleksandr Chernyshev <sanocher@gmail.com>
2018-05-31 01:02:33 +0500

    add Map, GameState

--- app/js/classes/Map.js
+++ app/js/classes/Map.js
@@ -1,7 +1,9 @@
 class Map {
     constructor() {
-        this.renderMap = new Array(30).fill(new Array(30).fill(0));
+        this.renderMap = new Array(30).fill(0).map(e => e = new Array(30).fill(0));
         this.init();
+        this.containerElement = $('#map');
+        this.ElementArray = [];
     }
 
     init() {
@@ -12,9 +14,38 @@ class Map {
         for (let i = 0; i < 16; i++) {
             let rX = randomInteger(0, this.renderMap[0].length - 1);
             let rY = randomInteger(0, this.renderMap.length - 1);
+
             this.renderMap[rY][rX] = 1;
-            console.log(rX, rY);
         }
         console.log(this.renderMap);
+    }
+
+    render(isShow) {
+        let chooseClass = (value) => {
+            if (!isShow) {
+                return 'unknow';
+            }
+            switch (value) {
+                case 0:
+                    return 'empty';
+                    break;
+                case 1:
+                    return 'minion';
+                    break;
+                default:
+                    return 'empty';
+                    break;
+            }
+        }
+        let html = '';
+        for (var y = 0; y < this.renderMap.length; y++) {
+            html += '<tr>'
+            for (var x = 0; x < this.renderMap[0].length; x++) {
+                let className = chooseClass(this.battleground[y][x]);
+                html += `<td id="${x + '&' + y}" class="${className}" data-x=${x} data-y=${y}></td>`;
+            }
+            html += '</tr>';
+        }
+        this.containerElement.html(`<table><tbody>${html}</tbody></table>`)
     }
 }
--- app/js/classes/Minions.js
+++ app/js/classes/Minions.js
@@ -1,38 +1,38 @@
-class Element{
-    constructor(name, position){
+class Element {
+    constructor(name, position) {
         this.name = name;
         this.position = position;
     }
 }
 
-class Poison extends Element{
-    constructor(position){
+class Poison extends Element {
+    constructor(position) {
         super("Poison", position);
         this.damage = 10;
     }
 }
 
-class Food extends Element{
-    constructor(position){
+class Food extends Element {
+    constructor(position) {
         super("Food", position);
         this.amoutOfHeal = 10;
     }
 }
 
-class Wall extends Element{
-    constructor(position){
+class Wall extends Element {
+    constructor(position) {
         super("Wall", position);
     }
 }
 
-class Empty extends Element{
-    constructor(position){
+class Empty extends Element {
+    constructor(position) {
         super("Empty", position);
     }
 }
 
-class Minion extends Element{
-    constructor(position, genom){
+class Minion extends Element {
+    constructor(position, genom) {
         super("Minion", position);
         this.hp = 50;
         this.maxHp = 100;
@@ -41,21 +41,20 @@ class Minion extends Element{
         this.isDead = false;
     }
 
-    setHp(count){
-         probablyHp = this.hp+count;
-         if(probablyHp < 0){
-             this.isDead = true;
-         }
-         if(probablyHp < this.maxHp)
-         {
-             this.hp = probablyHp;
-         }
-         if(probablyHp >= this.maxHp){
-             this.hp = this.maxHp;
-         };
+    setHp(count) {
+        probablyHp = this.hp + count;
+        if (probablyHp < 0) {
+            this.isDead = true;
+        }
+        if (probablyHp < this.maxHp) {
+            this.hp = probablyHp;
+        }
+        if (probablyHp >= this.maxHp) {
+            this.hp = this.maxHp;
+        };
     }
-    nextIp(count){
+    nextIp(count) {
         let newIp = (this.ip + count) % 56;
         return newIp;
     }
-} 
+}
